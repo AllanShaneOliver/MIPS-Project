15671b891e635437aa43dd96bed446501748405a
Shane Oliver <shaneoliver@Shanes-Air.fios-router.home>
2020-10-15 23:34:00 -0400

    Initialized character variables

--- project1.s
+++ project1.s
@@ -11,3 +11,12 @@ main:
   li $a1, 10 #input no more than 10 characters
   syscall
   move $t1, $a0
+
+li $s1, '0' #character 0
+li $s2, '9' #character 9
+li $t4, 'a' #character a - lower boundary for base 29
+li $t5, 's' #character s - upper boundary for base 29
+li $t6, 'A' #character A - lower boundary for base 29
+li $t7, 'S' #character S - upper boundary for base 29
+li $s3, 0 #result variable
+li $s4, 0 #variable kept at 0
