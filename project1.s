f4c71b3da99f12b57294c85bb782a9be9a40f0a2
Shane Oliver <shaneoliver@Shanes-Air.fios-router.home>
2020-10-15 20:46:26 -0400

    MIPS Project I Test

---
+++ project1.s
@@ -0,0 +1,10 @@
+.data
+#PritStat: .asciiz "Using my Howard student ID Number, I determined that (02940666 % 11) + 10 is 29. Therefore, all conversions will be completed from Base 29. \n Please input the number you wish to convert from Base 29 to decimal: \n"
+list: .space 11 #can store 10 characters into a string named "list"
+
+.text
+.globl main
+
+#git add .
+#git commit -m "description"
+#git push
